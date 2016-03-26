dc497b33a8e502a2d7956a725623ef9aaa1b9efb
iwasa mitsuhiro <iwasa.mitsuhiro@gmail.com>
2016-03-27 00:07:21 +0900

    initially import

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+var webpack = require('webpack');
+
+module.exports = {
+  entry: {
+    main: './src/main.ts',
+    vendor: [
+      'reflect-metadata',
+      'zone.js',
+      'es6-shim',
+      'angular2/core',
+      'angular2/platform/browser',
+      'angular2/http',
+      'angular2/router',
+      'rxjs/add/operator/map',
+      'rxjs/add/operator/mergeMap'
+    ]
+  },
+  output: {
+    filename: 'main.js'
+  },
+  resolve: {
+    // Add `.ts` and `.tsx` as a resolvable extension.
+    extensions: ['', '.webpack.js', '.web.js', '.ts', '.tsx', '.js']
+  },
+  module: {
+    loaders: [
+      // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
+      { test: /\.tsx?$/, loader: 'ts-loader' }
+    ]
+  },
+  devtool: '#source-map',
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin(/* chunkName= */"vendor", /* filename= */"vendor.bundle.js"),
+    new webpack.ProvidePlugin({
+            // 'reflect-metadata': 'reflect-metadata',
+            // 'Promise': 'imports?this=>global!exports?global.Promise!es6-promise', // Thanks Aaron (https://gist.github.com/Couto/b29676dd1ab8714a818f#gistcomment-1584602)
+            // 'fetch': 'imports?this=>global!exports?global.fetch!whatwg-fetch'
+        }),
+  ]
+}
